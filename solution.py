ed4333db29bdde1099e4a8efacf806ae40ea52bc
FareedRehan <fareed.rehan@dci-student.org>
2023-08-22 17:19:36 +0200

    answers added

--- solution.py
+++ solution.py
@@ -2,46 +2,45 @@
 # Convert a decimal number to binary
 
 num = 27
-binary = bin(___)
+binary = bin(27)
 print(binary) # 0b11011
 
 
 # Task2
 # Convert a binary number to decimal
 
-num = ______
-decimal = ___(num)
+num = 0b11011
+decimal = print(0b11011)
 print(decimal) # 27
-
 
 # Task3
 # Convert a decimal number to hexadecimal
 
 num = 27
-hexadecimal = hex(___)
-print(____) # 0x1b
+hexadecimal = hex(27)
+print(hexadecimal) # 0x1b
 
 # Task4
 # Convert a hexadecimal number to decimal
 
-num = ___
-decimal = _____
-print(____) # 27
+num = 0x1b
+decimal = 0x1b
+print(decimal) # 27
 
 
 # Task5
 # Convert a binary number to hexadecimal
 
-num = ________
-hexadecimal = _______
+num = 0b11011
+hexadecimal = hex(0b11011)
 print(hexadecimal) # 0x1b
 
 # Task6
 # Convert a hexadecimal number to binary
 
-num = ____
-____ = bin(____)
-print(____) # 0b11011
+num = 0x1b
+binary = bin(0x1b)
+print(binary) # 0b11011
 
 # Bonus
 
@@ -49,13 +48,15 @@ print(____) # 0b11011
 # Convert a decimal number to octal
 
 num = 27
-____= ____
+octal= oct(27)
 print(octal) # 0o33
 
 ### Task 8
 # Convert a octal number to decimal
 
 num = 0o33
+decimal= 0o33
+print(decimal)
 
 
 
